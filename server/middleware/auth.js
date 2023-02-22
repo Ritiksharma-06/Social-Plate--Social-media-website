c3a279bfdc5ef917afdffb0a7504a22e27b39afb
Ritik Sharma <111641844+Ritiksharma-06@users.noreply.github.com>
2023-02-22 23:29:28 +0530

    MERN Social media app

---
+++ server/middleware/auth.js
@@ -0,0 +1,43 @@
+// import { json } from 'body-parser';
+// import jwt from 'jsonwebtoken';
+
+// export const verifyToken = async (req, res, next) => {
+//     try{
+//         let token = req.header("authorization");
+//         if(!token) {
+//             return res.status(403).send("Access Denied");
+//         }
+
+//         if(token.startWith("Bearer")){
+//             token = token.slice(7, token.length).trimLeft();
+//         }
+
+//         const verified = jwt.verify(token, process.env.JWT_SECRET);
+//         req.user = verified;
+//         next();
+//     } catch (err) {
+//         res.status(500).json({ error: err.message })
+//     }
+// }
+
+import jwt from "jsonwebtoken";
+
+export const verifyToken = async (req, res, next) => {
+  try {
+    let token = req.header("Authorization");
+
+    if (!token) {
+      return res.status(403).send("Access Denied");
+    }
+
+    if (token.startsWith("Bearer ")) {
+      token = token.slice(7, token.length).trimLeft();
+    }
+
+    const verified = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = verified;
+    next();
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
